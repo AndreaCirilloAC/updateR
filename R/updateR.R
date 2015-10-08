3192990b04302a6480f878c87153b2b58f58c52b
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-10-08 23:14:33 +0200

    introduced html_text to extract version name

--- R/updateR.R
+++ R/updateR.R
@@ -4,7 +4,9 @@ library(dplyr)
 
 #compose url ( get last version using a bit of web scraping)
 page_source <- read_html("https://cran.rstudio.com/bin/macosx/")
-html_nodes(page_source,"table:nth-child(7) tr:nth-child(1) td:nth-child(1)")
+version     <- html_nodes(page_source,"table:nth-child(7) tr:nth-child(1) td:nth-child(1)")
+html_text(version)
+# ADESSO RIMUOVI LA PARTE NON NECESSARIA ED HAI ESTRATTO IL LINK
 
 #download package, set folder for download
 system("curl -o 'R-3.2.2.pkg' 'https://cran.rstudio.com/bin/macosx/R-3.2.2.pkg'")

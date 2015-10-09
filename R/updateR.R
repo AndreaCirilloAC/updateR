e59a3c213dd6bdc17aa0e38ce692790d2cbe3d67
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-10-09 21:30:08 +0200

    introduced filename_quoted in order to have a version of filname usable even for url construction filename_quoted is defined

--- R/updateR.R
+++ R/updateR.R
@@ -11,19 +11,19 @@ stopifnot(.Platform$OS.type == "unix")
 
 page_source       <- read_html("https://cran.rstudio.com/bin/macosx/")
 version_block     <- html_nodes(page_source,"table:nth-child(7) tr:nth-child(1) td:nth-child(1)")
-filename          <- html_text(version_block) %>% strsplit("/") # the resulting value is a list
+filename          <- html_text(version_block) %>% strsplit("\\", fixed = TRUE) # the resulting value is a list
 filename          <- filename[1] # we take the first element, containing the name of the file
-filename          <- paste("'",filename,"'",sep = "")
+filename_quoted          <- paste("'",filename,"'",sep = "")
 #everything went right?
 stopifnot(grepl(".pkg",version) == FALSE)
-url               <- paste('https://cran.rstudio.com/bin/macosx/R-3.2.2.pkg',filename, sep = "")
+url               <- paste('https://cran.rstudio.com/bin/macosx/',filename, sep = "")
 
 #download package, set folder for download
-command           <- paste("curl - o ", filename , " '",url,"'", sep = "")
+command           <- paste("curl - o ", filename_quoted , " '",url,"'", sep = "")
 system(command)
 #install .pkg file
 
-command           <- paste("installer -pkg ", filename , " -target / -verbose")
+command           <- paste("installer -pkg ", filename_quoted , " -target / -verbose")
 system(command)
 
 message("everything went smoothly")

8711819ad34103022c82c49fc663bec848b9a3ed
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-10-09 10:28:57 +0200

    first version complete script, to test

--- R/updateR.R
+++ R/updateR.R
@@ -1,17 +1,27 @@
 library(rvest)
 library(dplyr)
-## scratch
+library(assertthat)
+
 
 #we scrape CRAN page to retrieve the last R version and compose dowloading URL
+
 page_source       <- read_html("https://cran.rstudio.com/bin/macosx/")
 version_block     <- html_nodes(page_source,"table:nth-child(7) tr:nth-child(1) td:nth-child(1)")
-version           <-html_text(version_block) %>% strsplit("/")
-
-
-
-# ADESSO RIMUOVI LA PARTE NON NECESSARIA ED HAI ESTRATTO IL LINK
+filename          <- html_text(version_block) %>% strsplit("/") # the resulting value is a list
+filename          <- filename[1] # we take the first element, containing the name of the file
+filename          <- paste("'",filename,"'",sep = "")
+#everything went right?
+stopifnot(grepl(".pkg",version) == FALSE)
+url               <- paste('https://cran.rstudio.com/bin/macosx/R-3.2.2.pkg',filename, sep = "")
 
 #download package, set folder for download
-system("curl -o 'R-3.2.2.pkg' 'https://cran.rstudio.com/bin/macosx/R-3.2.2.pkg'")
+command           <- paste("curl - o ", filename , " '",url,"'", sep = "")
+system(command)
 #install .pkg file
-system("installer -pkg 'R-3.2.2.pkg' -target / -verbose")
+
+command           <- paste("installer -pkg ", filename , " -target / -verbose")
+system(command)
+
+message("everything went smoothly")
+message("open a Terminal session and run 'R' to assert that last version was installed")
+

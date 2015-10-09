27c682625eb8b76a67cfbfda367f94a3782820c3
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-10-09 21:42:57 +0200

    solved issue with strplit, escaping with "\n"

--- R/updateR.R
+++ R/updateR.R
@@ -11,19 +11,20 @@ stopifnot(.Platform$OS.type == "unix")
 
 page_source       <- read_html("https://cran.rstudio.com/bin/macosx/")
 version_block     <- html_nodes(page_source,"table:nth-child(7) tr:nth-child(1) td:nth-child(1)")
-filename          <- html_text(version_block) %>% strsplit("\\", fixed = TRUE) # the resulting value is a list
-filename          <- filename[1] # we take the first element, containing the name of the file
+filename          <- html_text(version_block) %>% strsplit("\n", fixed = TRUE) # the resulting value is a list
+filename          <- filename[[1]]
+filename          <- filename[[1]] # we take the first element, containing the name of the file
 filename_quoted          <- paste("'",filename,"'",sep = "")
 #everything went right?
 stopifnot(grepl(".pkg",version) == FALSE)
 url               <- paste('https://cran.rstudio.com/bin/macosx/',filename, sep = "")
 
 #download package, set folder for download
-command           <- paste("curl - o ", filename_quoted , " '",url,"'", sep = "")
+command           <- paste("curl -o ", filename_quoted , " '",url,"'", sep = "")
 system(command)
 #install .pkg file
 
-command           <- paste("installer -pkg ", filename_quoted , " -target / -verbose")
+command           <- paste("sudo installer -pkg ", filename_quoted , " -target / -verbose")
 system(command)
 
 message("everything went smoothly")

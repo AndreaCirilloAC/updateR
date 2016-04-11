d88782b4b239439e99cb88bcd09a267cf0eed03a
AndreaCirilloAC <andreacirilloac@gmail.com>
2016-04-11 22:40:05 +0200

    minor formatting

--- R/updateR.R
+++ R/updateR.R
@@ -3,29 +3,28 @@ updateR <- function(admin_password = "", page_source = "https://cran.rstudio.com
   pacman::p_load(rvest,dplyr)
 
 #first test for on OS
-
 stopifnot(.Platform$OS.type == "unix")
+
 #we scrape CRAN page to retrieve the last R version and compose dowloading URL
-
-
 page              <- read_html(page_source)
 version_block     <- rvest::html_nodes(page,"h1+ p a+ a , table:nth-child(8) tr:nth-child(1) td > a")
 filename          <- rvest::html_text(version_block) %>% strsplit("\n", fixed = TRUE) # the resulting value is a list
 filename          <- filename[[2]] # we take the first element, containing the name of the file
 filename_quoted          <- paste("'",filename,"'",sep = "")
-#everything went right?
 
+#everything went right?
 stopifnot(grepl(".pkg",version) == FALSE)
 url               <- paste(page_source,filename, sep = "")
 
 #download package, set folder for download
 command           <- paste("curl -o -v ", filename_quoted , " '",url,"'", sep = "")
 system(command)
-#install .pkg file
 
+#install .pkg file
 command           <- paste("echo " , admin_password, "| sudo -S installer -pkg ", filename_quoted , " -target / -verbose")
 system(command)
 
+#congratulate with the reader
 message("everything went smoothly")
 message("open a Terminal session and run 'R' to verify that latest version was installed")
 }

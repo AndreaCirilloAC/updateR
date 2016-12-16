b89c97dd34b9ca9762e038b2e2b2c5f1207114f4
AndreaCirilloAC <andreacirilloac@gmail.com>
2016-12-16 22:43:55 +0100

    fixed namespace error on read_html referencing it to xml2

--- R/updateR.R
+++ R/updateR.R
@@ -5,14 +5,14 @@ library(dplyr)
 stopifnot(.Platform$OS.type == "unix")
 
 #we scrape CRAN page to retrieve the last R version and compose dowloading URL
-page              <- rvest::read_html(page_source)
+page              <- xml2::read_html(page_source)
 version_block     <- rvest::html_nodes(page,"h1+ p a+ a , table:nth-child(8) tr:nth-child(1) td > a")
 filename          <- rvest::html_text(version_block) %>% strsplit("\n", fixed = TRUE) # the resulting value is a list
 filename          <- filename[[2]] # we take the second element, containing the name of the file
 filename_quoted          <- paste("'",filename,"'",sep = "")
 
 #everything went right?
-assertthat::stopifnot(grepl(".pkg",version) == FALSE)
+stopifnot(grepl(".pkg",version) == FALSE)
 url               <- paste(page_source,filename, sep = "")
 
 #download package, set folder for download

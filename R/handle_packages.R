be435714baf9bcf35fd3bf4f4450fe1b4d9dbdf3
Chuck <chuckleong21@gmail.com>
2020-07-24 22:23:23 +0800

    Added helper functions

---
+++ R/handle_packages.R
@@ -0,0 +1,69 @@
+#' @noRd
+
+utils::globalVariables(c("Package", "Priority", "installing"))
+
+#' @noRd
+list_packages <- function() {
+  all_pkg <- installed.packages() %>%
+    as.data.frame() %>%
+    pull(Package)
+  base_pkg <- installed.packages() %>%
+    as.data.frame() %>%
+    filter(Priority == "base") %>%
+    pull(Package)
+  all_pkg[!all_pkg %in% base_pkg]
+}
+
+#' @noRd
+restore_packages <- function(status = latest_r_version()) {
+  # detect update type
+  versions <- c(status$latest, attr(status, "current"))
+  versions_numeric <- as.numeric(sub("^(\\d)\\.", "\\1", versions))
+  versions_change <- abs(diff(floor(versions_numeric / 10)))
+  update_type <- ifelse(versions_change > 0, "major",
+                        ifelse(abs(diff(versions_numeric)) >= 1,
+                               "minor", "patch")
+  )
+
+  # prompt restore options
+  prompt_msg <- paste(c("This is a %s update.",
+                        "Choose one of the following options to restore your packages:",
+                        "%s\n"),
+                      collapse = "\n")
+  prompt_options <- switch(update_type,
+                           "major" = "1. Reinstall all the packages\n",
+                           "minor" = paste(
+                             c("1. Reinstall all the packages",
+                               "2. Copy all packages"),
+                             collapse = "\n"),
+                           "patch" = paste(
+                             c("Restoring packages is NOT necessary.",
+                               "Press [Enter] to continue"),
+                             collapse = "\n"))
+  prompt_msg <- sprintf(prompt_msg, update_type, prompt_options)
+  lib <- "/Library/Frameworks/R.framework/Versions/%.1f/Resources/library"
+  choice <- as.numeric(readline(prompt_msg))
+
+  # handling options
+  while(!choice %in% c(1, 2, "")) {
+    message("!Invalid option. Please try again\n")
+    choice <- as.numeric(readline(prompt_msg))
+  }
+
+  if(choice == 1) {
+    # reinstall
+    message("list of packages loaded")
+    cat(sprintf("%s,", installing))
+    install.packages(installing)
+  } else if(update_type == "minor" & choice == 2) {
+    # copy
+    old <- sprintf(lib, floor(min(versions_numeric)) / 10)
+    new <- sprintf(lib, floor(max(versions_numeric)) / 10)
+    message(sprintf("Copying from old LibPath\n%s/\nto new LibPath\n%s/\n...",
+                    old, new))
+    system(sprintf("cp -R %s/ %s/", old, new))
+    message("Complete.")
+  } else if(update_type == "patch") {
+    message("\n")
+  }
+}
---
+++ R/utils.R
@@ -0,0 +1,62 @@
+#' @noRd
+latest_r_version <- function() {
+        cran <- "http://cran.rstudio.com/bin/macosx/"
+        version_regex <- "(\\d+)?\\.(\\d+)?\\.(\\d+)?"
+        page <- read_html(cran)
+        file_url <- page %>%
+                html_nodes(xpath = "//td/a") %>% .[1] %>%
+                html_attr("href") %>%
+                paste(cran, ., sep = "")
+        minimal <- page %>%
+                html_nodes(xpath = '//table[1]//tr[1]//td[2]') %>%
+                html_text() %>%
+                trimws() %>%
+                regmatches(., regexpr("macOS.*higher.", .)) %>%
+                regmatches(., regexpr("\\d+.\\d+", .))
+        r_latest <- regmatches(file_url, regexpr(version_regex, file_url))
+        r_current <- paste(version$major, version$minor, sep = ".")
+        r_latest_numeric <- as.numeric(sub("^(\\d)\\.", "\\1", r_latest))
+        r_current_numeric <- as.numeric(sub("^(\\d)\\.", "\\1", r_current))
+
+        structure(
+                list(update_avail = ifelse(r_latest_numeric > r_current_numeric, T, F),
+                     latest = r_latest,
+                     url = file_url),
+                current = paste(version$major, version$minor, sep = "."),
+                OS_minimal = as.numeric(minimal)
+        )
+}
+
+#' @noRd
+ask_password <- function() {
+        whoami <- system2("whoami", stdout = TRUE)
+        readline(sprintf("Enter password for %s: ", whoami))
+}
+
+#' @noRd
+check_compactability <- function(status = latest_r_version()) {
+        e <- "Latest R Version %s requires macOS at least 10.13 or higher,
+        but %.2f is running. Consider upgrading macOS or install legacy R binaries from
+        http://cran.rstudio.com/bin/macosx/"
+        ver_major_minor <- as.numeric(sub("\\.", "",
+                                          sub("^(.\\..).*$", "\\1", status$latest)))
+        macOS <- sessionInfo()$running %>%
+                regmatches(., regexpr("(\\d+)\\.(\\d+)\\.(\\d+)$", .)) %>%
+                sub("^(.+\\.(.+)?)\\..+", "\\1", x = .) %>%
+                as.numeric()
+
+        if(isTRUE(status$update_avail) && ver_major_minor >= 40) {
+                compactible <- macOS >= attr(status, "OS_minimal")
+        }
+
+        if(!compactible) stop(sprintf(e, status$latest, macOS), call. = FALSE)
+}
+
+
+#' @noRd
+package_exists <- function(status = latest_r_version()) {
+        pkg_name <- regmatches(status$url, regexpr("R.*$", status$url))
+        download.path <- sprintf("/Users/%s/Downloads/",
+                                 system2("whoami", stdout = TRUE))
+        pkg_name %in% list.files(download.path)
+}

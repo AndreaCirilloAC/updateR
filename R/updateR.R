887c0c67167f1a355416ac0c534d69837c120dd6
Chuck <chuckleong21@gmail.com>
2020-07-24 22:26:03 +0800

    Update with helper functions

--- R/updateR.R
+++ R/updateR.R
@@ -1,62 +1,43 @@
 #' @importFrom xml2 read_html
-#' @importFrom rvest html_nodes
-#' @importFrom rvest html_text
-#' @importFrom utils download.file
-#' @importFrom stringr str_extract_all
-#' @importFrom dplyr select
+#' @importFrom rvest html_nodes html_text html_attr
+#' @importFrom utils download.file installed.packages install.packages
+#' @importFrom utils sessionInfo
+#' @importFrom dplyr select filter pull
 #' @title Downloads and installs the latest version of R for Mac OS X.
 #' @description Update your version of R from inside R itself (Mac OS X only).
-#' @param admin_password \code{character}. The system-wide password of the user. The parameter will be only employed to execute commands gaining administrator privileges on the computer and will not be stored anywhere.
 #' @author Andrea Cirillo, Robert Myles McDonnell
 #' @examples
-#' updateR(admin_password = "****")
+#' \dontrun{
+#' ## NOT RUN
+#' updateR()
+#' ## END NOT RUN
+#' }
 #' @export
-updateR <- function(admin_password = NULL, file = NA){
+updateR <- function(){
 
   # first test for on OS
   stopifnot(.Platform$OS.type == "unix")
-  # test for password
-  if ( is.null(admin_password)){
-    stop("User system password is missing")
-  }
 
-  installed.packages() %>%
-  as.data.frame() %>%
-  select(Package) %>%
-  as.vector() -> needed_packages # saving packages installed before updating R version
-  needed_packages <- paste(unlist(needed_packages))
-  save(needed_packages, file = "/tmp/needed_packages.RData")
-
-  page_source = "https://cran.rstudio.com/bin/macosx/"
-
-  css <- "body > table"
-if (is.na(file)){
-  file <- xml2::read_html(page_source) %>%
-    rvest::html_nodes(css) %>%
-    rvest::html_text() %>%
-    stringr::str_extract_all(pattern = "^[:print:]*\\.pkg") %>%
-    .[[1]]
-}
-
-  stopifnot(grepl(".pkg", file) == TRUE)
-
-  latest_version <- as.numeric(paste(stringr::str_extract_all(pattern = "[:digit:]{1}",file)[[1]],collapse=""))
-  installed_version <- as.numeric(paste(stringr::str_extract_all(pattern = "[:digit:]{1}",paste(version$major,version$minor))[[1]],collapse=""))
-  if (installed_version >= latest_version) {
-    message(paste("Update not necessary. Latest version ====",version$version.string,"==== already installed."))
+  latest <- latest_r_version()
+  if(!latest$update_avail) {
+    message(paste("Update not necessary. Latest version ====",
+                  version$version.string, "==== already installed."))
     return()
   }
+  check_compactability(status = latest)
+  installing <- list_packages()
 
-  url <- paste0(page_source, file)
-
-  destpath <- paste(getwd(),"/",sep = "")
-  fullpath <- paste(destpath,file,sep = "")
+  folderpath <- sprintf("/Users/%s/Downloads/",
+                      system2("whoami", stdout = TRUE))
+  pkgfile <- regmatches(latest$url, regexpr("R.*$", latest$url))
+  fullpath <- sprintf("%s%s", folderpath, pkgfile)
   # download package, set folder for download
-  download.file(url, fullpath)
+  if(!package_exists(status = latest))
+    download.file(latest$url, fullpath)
 
   #install .pkg file
-  pkg <- gsub("\\.pkg" , "", file)
-  message(paste0("Installing ", pkg, "...please wait"))
+  admin_password <- ask_password()
+  message(paste0("Installing R-", latest$latest, "...please wait"))
 
   command <- paste0("echo ", admin_password, " | sudo -S installer -pkg ",
                 "'", fullpath, "'", " -target /")
@@ -65,11 +46,8 @@ if (is.na(file)){
   arg <- paste0("--check-signature ", fullpath)
   system2("pkgutil", arg)
 
-  # install back the packages saved at the beginning of the process
-  load("/tmp/needed_packages.RData", verbose = TRUE)
-  message("list of packages loaded")
-  needed_packages
-  install.packages(as.vector(needed_packages))
+  # restore packages
+  restore_packages(status = latest)
 
   # store version of R
   x <- system2("R", args = "--version", stdout = TRUE)
